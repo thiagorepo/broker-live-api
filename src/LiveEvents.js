f1621d33171c221ce6747325a1c28025448e291b
thiagorepo <thiagodeepcoder+github@gmail.com>
2026-01-28 20:59:12 -0300

    v2

--- src/LiveEvents.js
+++ src/LiveEvents.js
@@ -1,40 +1,57 @@
-type LivEventHandler = (msgData: Object) => void;
+type LivEventHandler = (msgData: any) => void;
 
-export default class LiveEvents {
-    messageHandlers: Object;
+export class ImprovedLiveEvents {
+  messageHandlers: Record<string, LivEventHandler[]>;
 
-    constructor() {
-        this.messageHandlers = {};
-    }
+  constructor() {
+      this.messageHandlers = {};
+  }
 
-    emitSingle(msgType: string, msgData: Object) {
-        const handlers = this.messageHandlers[msgType] || [];
-        handlers.forEach(handler => {
-            handler(msgData);
-        });
-    }
+  emitSingle(msgType: string, msgData: any) {
+      const handlers = this.messageHandlers[msgType] || [];
+      handlers.forEach(handler => {
+          handler(msgData);
+      });
+  }
 
-    emitWildcard(msgData: Object) {
-        const handlers = this.messageHandlers['*'] || [];
-        handlers.forEach(handler => {
-            handler(msgData);
-        });
-    }
+  emitWildcard(msgData: any) {
+      const handlers = this.messageHandlers['*'] || [];
+      handlers.forEach(handler => {
+          handler(msgData);
+      });
+  }
 
-    emit(msgType: string, msgData: Object) {
-        this.emitSingle(msgType, msgData);
-        this.emitWildcard(msgData);
-    }
+  emit(msgType: string, msgData: any) {
+      this.emitSingle(msgType, msgData);
+      this.emitWildcard(msgData);
+  }
 
-    on(msgType: string, callback: LivEventHandler) {
-        if (!this.messageHandlers[msgType]) {
-            this.messageHandlers[msgType] = [callback];
-        } else {
-            this.messageHandlers[msgType].push(callback);
-        }
-    }
+  on(msgType: string, callback: LivEventHandler) {
+      if (!this.messageHandlers[msgType]) {
+          this.messageHandlers[msgType] = [callback];
+      } else {
+          this.messageHandlers[msgType].push(callback);
+      }
+  }
 
-    ignoreAll(msgType: string) {
-        delete this.messageHandlers[msgType];
-    }
+  ignoreAll(msgType: string) {
+      delete this.messageHandlers[msgType];
+  }
+
+  // --- Extensions for cleanup ---
+
+  off(msgType: string, callback: LivEventHandler) {
+      if (!this.messageHandlers[msgType]) return;
+      this.messageHandlers[msgType] = this.messageHandlers[msgType].filter(
+          cb => cb !== callback
+      );
+  }
+
+  removeAllListeners(msgType?: string) {
+      if (msgType) {
+          delete this.messageHandlers[msgType];
+      } else {
+          this.messageHandlers = {};
+      }
+  }
 }
